d74289d01ae90931b7e206092f69fca396ce8cf8
shubhranshu789 <66314743+shubhranshu789@users.noreply.github.com>
2025-03-19 23:09:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 
-
-const port = const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 
@@ -16,8 +15,8 @@ app.use(express.json());
 app.use(require('./routes/album'))
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
 
 
